48f8e12e0a7d9e32d67248a5e35523be3ef617c1
stim.ai <leytzher@gmail.com>
2021-11-13 20:05:46 +0400

    debug

--- migrations.go
+++ migrations.go
@@ -22,9 +22,8 @@ type CoinListing struct {
 }
 
 func InitialMigration(){
-	err:= godotenv.Load()
-	b := err != nil
-	if b {
+	errs:= godotenv.Load()
+	if errs != nil {
 		log.Fatal("Error loading .env file")
 	}
 	user := os.Getenv("POSTGRES_USER")
@@ -32,12 +31,11 @@ func InitialMigration(){
 	db := os.Getenv("POSTGRES_DB")
 	host := os.Getenv("POSTGRES_HOST")
 	dns := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Shanghai", host,user, passwd, db)
-	fmt.Println(dns)
 	DB, err := gorm.Open(postgres.Open(dns), &gorm.Config{})
 
 	if err != nil {
-	fmt.Println(err.Error())
-	panic("Cannot connect to DB")
+		fmt.Println(err.Error())
+		panic("Cannot connect to DB")
 	}
 	DB.AutoMigrate(&CoinListing{})
 
